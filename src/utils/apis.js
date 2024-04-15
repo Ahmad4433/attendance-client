03f6b6abb0c8f363bf5ac430d0a0a00988b7a675
Ahmad4433 <aliraza126005#gmail.com>
2024-04-15 21:22:13 +0500

    dsds

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,5 +1,5 @@
 const apis = () => {
-  const baseUrl = "http://localhost:8000/";
+  const baseUrl = "https://attendance-server-029ac8e3ecf3.herokuapp.com/";
   return {
     markAttendance: `${baseUrl}user/attendance`,
     getLocation:`${baseUrl}get/location`
